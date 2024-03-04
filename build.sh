bc4325fbddcd71ecc34523033194a9b191299931
s0urce-c0de <email.kanavg@gmail.com>
2024-03-04 13:55:22 -0800

    add rustup

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 cd "$(dirname "$0")"
 # Submodules
 git submodule update --init
+# wasm, otherwise npm gives an ugly error
+rustup target add wasm32-unknown-unknown
 # make
 if ! groups $USER | grep -q "docker"; then
   echo Please add yourself to group docker or don\'t build rootfs
